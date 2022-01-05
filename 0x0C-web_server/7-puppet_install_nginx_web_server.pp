568cdd237ae1961765c953f3ba67bb76b0970fe4
justmvrco <moreirapiliki@gmail.com>
2022-01-05 18:17:54 +0000

    add task 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# install and set up nginx
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+file { '/var/www/html/index.html':
+  require => Package['nginx'],
+  content => 'Hello World!',
+}
+
+file_line { 'add-rewrite':
+  ensure  => 'present',
+  require => Package['nginx'],
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'root /var/www/html;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+  notify  => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => File_line['add-rewrite'],
+}
